9b459c6a6abfe381cb2653690e4d65bc462f5d18
Sadeem Ansari <ansarisadeem8879@gmail.com>
2026-01-19 23:12:50 +0530

    commited

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-// document.addEventListener('DOMContentLoaded',function(){
-//     const card=document.querySelector('.card-hover');
-//     const buttons=document.querySelector('.button2');
 
-//     card.addEventListener('mouseover',function(){
-//         buttons.style.display='block';
-//     })
-
-//     card.addEventListener('mouseout',function(){
-//         button.style.display='none';
-//     })
-// })
 document.addEventListener('DOMContentLoaded', function() {
     const cards = document.querySelectorAll('.card-hover');
 
